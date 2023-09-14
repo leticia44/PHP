b1d12fec2c3683e4189d9842c1cfb8c827c0389d
12leticiasilva0990 <130669115+12leticiasilva0990@users.noreply.github.com>
2023-09-14 13:41:25 -0300

    comit lety

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $x=2;
+    $z=6;
+    $y=4;
+    $resultado=($x+$z)/$y-1;
+
+    if($resultado ==1)
+    {echo "verdadeiro";}
+    else {echo"falso";}
+
+?>
+</body>
+</html>
